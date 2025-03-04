f3b9cd8cacfa212cb43014aec496a86783c31833
Josh Clark <JoshClark885@outlook.com>
2025-03-04 23:34:04 +0000

    Projectile Weapon Logic
    Currently is not being called inside of blueprint so doesnt actually do anything

---
+++ Source/Group_13/Private/WeaponBase.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+class UArrowComponent;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FWeaponFireSignature);
+
+UCLASS(Abstract)
+class GROUP_13_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeaponBase();
+
+	UPROPERTY(BlueprintAssignable)
+	FWeaponFireSignature OnFire;
+
+	UFUNCTION(BlueprintCallable)
+	void StartFire();
+	UFUNCTION(BlueprintCallable)
+	void StopFire();
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<USceneComponent> _Root;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<USkeletalMeshComponent> _Mesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	TObjectPtr<UArrowComponent> _Muzzle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float _FireDelay;
+	FTimerHandle _FireDelayTimer;
+
+	UFUNCTION()
+	virtual void Fire();
+};
