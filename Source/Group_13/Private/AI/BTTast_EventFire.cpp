054209bb470ac8d7c1f767e208cf4f65a57f2e80
Donk-e <tywood722@gmail.com>
2025-03-04 00:16:32 +0000

    more ai and some code organising

---
+++ Source/Group_13/Private/AI/BTTast_EventFire.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTast_EventFire.h"
+
+#include "Pawnable.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+UBTTast_EventFire::UBTTast_EventFire()
+{
+	bNotifyTick = true;
+	bNotifyTaskFinished = true;
+	bCreateNodeInstance = false;
+
+	Key_Pawn.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTast_EventFire,Key_Pawn), APawn::StaticClass());
+}
+
+void UBTTast_EventFire::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	UBlackboardData* BBAsset = GetBlackboardAsset();
+	if(ensure(BBAsset))
+	{
+		Key_Pawn.ResolveSelectedKey(*BBAsset);
+	}
+}
+
+EBTNodeResult::Type UBTTast_EventFire::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const UBlackboardComponent* BBComp = OwnerComp.GetBlackboardComponent();
+	UObject* pawn = BBComp->GetValueAsObject(Key_Pawn.SelectedKeyName);
+	if(UKismetSystemLibrary::DoesImplementInterface(pawn, UPawnable::StaticClass()))
+	{
+		//IPawnable
+	}
+	
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
+
+EBTNodeResult::Type UBTTast_EventFire::AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::AbortTask(OwnerComp, NodeMemory);
+}
---
+++ Source/Group_13/Private/AI/BTTast_EventFire.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTast_EventFire.generated.h"
+
+UCLASS()
+class GROUP_13_API UBTTast_EventFire : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+public:
+	UBTTast_EventFire();
+
+	virtual void InitializeFromAsset(UBehaviorTree& Asset) override;
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	UPROPERTY(EditAnywhere, Category= "Blackboard")
+	FBlackboardKeySelector Key_Pawn;
+};
