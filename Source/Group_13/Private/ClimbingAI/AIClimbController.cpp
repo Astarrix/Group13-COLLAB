7488ea1e7c475ceb5dd78921f44bb5d8f5840635
Donk-e <tywood722@gmail.com>
2025-03-31 12:52:44 +0100

    awful

---
+++ Source/Group_13/Private/ClimbingAI/AIClimbController.cpp
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AIClimbController.h"
+
+#include "../../../../Plugins/SpiderAIPlugin/spider-navigation-plugin-master/Source/SpiderNavigation/Public/SpiderNavigation.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+
+// Sets default values
+AAIClimbController::AAIClimbController()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AAIClimbController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	PossessPawn = InPawn;
+	RunBehaviorTree(GetBehaviourTree(InPawn));
+	//if wanna do home location do it in pawn?
+	//GetBlackboardComponent()->SetValueAsVector("HomeLocation", GetActorLocation());
+}
+
+// Called when the game starts or when spawned
+void AAIClimbController::BeginPlay()
+{	
+	Super::BeginPlay();
+
+	if(_SpiderNav)
+	{
+		FActorSpawnParameters spawnParams;
+		_SpiderNavRef = GetWorld()->SpawnActor<ASpiderNavigation>(_SpiderNav, spawnParams);
+	}
+	
+}
+
+// Called every frame
+void AAIClimbController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAIClimbController::Move(FVector Destination)
+{
+	AiLocation = PossessPawn->GetActorLocation();
+	MoveDesination = _SpiderNavRef->FindClosestNodeLocation(AiLocation);
+	CurrentPath = _SpiderNavRef->FindPath(AiLocation,MoveDesination,test);
+	CurrentPath.Remove(CurrentPath[0]);
+	//update local move
+	//|_> if local path found must move = true
+}
+
+void AAIClimbController::UpdateRotation()
+{
+	
+}
+
+void AAIClimbController::UpdateLocalMove()
+{
+	if(CurrentPath.Max()>0)
+	{
+		
+	}
+}
+
---
+++ Source/Group_13/Private/ClimbingAI/AIClimbController.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "AIClimbController.generated.h"
+
+class UBehaviorTreeComponent;
+
+UCLASS(Abstract)
+class GROUP_13_API AAIClimbController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAIClimbController();
+
+protected:
+	APawn* PossessPawn;
+	FVector AiLocation;
+	bool test;
+
+	FVector MoveDesination;
+	FVector LocalMove;
+	TArray<FVector> CurrentPath;
+	bool LocalPathFound;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<ASpiderNavigation> _SpiderNav;
+	
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly)
+	TObjectPtr<ASpiderNavigation> _SpiderNavRef;
+
+	//UBlackboardComponent* _Blackboard;
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	UBehaviorTreeComponent* GetBehaviourTree;
+	
+	
+	virtual void OnPossess(APawn* InPawn) override;
+	virtual void BeginPlay() override;
+	
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UFUNCTION()
+	void Move(FVector Destination);
+
+	UFUNCTION()
+	void UpdateRotation();
+
+	UFUNCTION()
+	void UpdateLocalMove();
+};
