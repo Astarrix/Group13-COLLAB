6cd461e9eb3598c98f10dd9ddea5cae0784b763a
Donk-e <tywood722@gmail.com>
2025-04-03 02:02:22 +0100

    started spawner :D

---
+++ Source/Group_13/Private/ClimbingAI/AiSpawner.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AiSpawner.h"
+
+#include "AIPawn.h"
+
+
+// Sets default values
+AAiSpawner::AAiSpawner()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAiSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAiSpawner::SpawnBug()
+{
+	UWorld* const world = GetWorld();
+	if(world == nullptr || _AiClass == nullptr) {return;}
+
+	FActorSpawnParameters spawnParams;
+	//world->SpawnActor(_AiClass, )
+}
+
+void AAiSpawner::Handle_HealthDamaged(float current, float max, float change)
+{
+}
+
+void AAiSpawner::Handle_HealthDead(AController* causer)
+{
+	Destroy();
+}
+
+
+
---
+++ Source/Group_13/Private/ClimbingAI/AiSpawner.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AiSpawner.generated.h"
+
+class AAIPawn;
+class UHealthComponent;
+
+UCLASS(Abstract)
+class GROUP_13_API AAiSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAiSpawner();
+
+	void SpawnBug();
+
+protected:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSubclassOf<AAIPawn> _AiClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UHealthComponent> _Health;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override; 
+
+	UFUNCTION()
+	void Handle_HealthDamaged(float current, float max, float change);
+	UFUNCTION()
+	void Handle_HealthDead(AController* causer);
+};
