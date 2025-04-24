efa9d5d629777c991992e7d7e504a47d223b2a6e
Astarrix <60230113+Astarrix@users.noreply.github.com>
2025-04-24 20:32:45 +0100

    tutorial thing!
    added a tutorial system where a new tutorial node can be called which can spawn an on screen tutorial text, and has an optional extra for allowing events to run before and after a tutorial is spawned in the same way that the objectives work

--- Source/Group_13/Private/FunctionLibrary.cpp
+++ Source/Group_13/Private/FunctionLibrary.cpp
@@ -3,6 +3,8 @@
 
 #include "FunctionLibrary.h"
 
+#include "Blueprint/WidgetBlueprintLibrary.h"
+
 int UFunctionLibrary::LeftBitShift(int data, int pos)
 {
 	int shifted = (data << pos);
@@ -15,12 +17,11 @@ int UFunctionLibrary::RightBitShift(int data, int pos)
 	return shifted;
 }
 
-void UFunctionLibrary::GetActorAxis(AActor* actor, FVector &Position, FVector& Forward, FVector& Right, FVector& Up)
+void UFunctionLibrary::GetActorAxis(AActor* actor, FVector& Position, FVector& Forward, FVector& Right, FVector& Up)
 {
 	Position = actor->GetActorLocation();
 	Forward = actor->GetActorForwardVector();
 	Right = actor->GetActorRightVector();
 	Up = actor->GetActorUpVector();
 }
-
 
