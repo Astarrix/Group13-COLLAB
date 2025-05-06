b560e74dd92495675d6f6255d6d0af440eebf7f5
Astarrix <60230113+Astarrix@users.noreply.github.com>
2025-05-06 12:21:57 +0100

    nightmare nightmare nightmare

--- Source/Group_13/Group_13.Build.cs
+++ Source/Group_13/Group_13.Build.cs
@@ -8,7 +8,7 @@ public class Group_13 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
